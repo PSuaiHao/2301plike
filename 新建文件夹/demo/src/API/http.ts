122c339cd9c985b47b9c5b4758f0ed70a2c76f0a
PSuaiHao <2217441005@qq.com>
2023-11-02 16:01:30 +0800

    新建文件夹

--- 新建文件夹/demo/src/API/http.ts
+++
@@ -1,7 +0,0 @@
-import request from "./request";
-export const querybanner = (params: any) => request.get('/banner/list', params)
-export const querylist = (data: any) => request.post('/shop/goods/list/v2', data)
-export const querydetail = (id: any) => request.post('/shop/goods/detail?id='+id)
-export const queryleft = () => request.get('/shop/goods/category/all')
-export const queryright = (data: any) => request.post('/shop/goods/list/v2', data)
-export const queryadd=(data:any)=>request.post('shopping-cart/add',data)
