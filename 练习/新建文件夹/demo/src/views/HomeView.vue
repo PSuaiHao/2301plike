1e9cd209db7c20dc42bda90a8dc91a1aaae1ff19
PSuaiHao <2217441005@qq.com>
2023-11-02 20:09:00 +0800

    首页

--- 练习/新建文件夹/demo/src/views/HomeView.vue
+++ 练习/新建文件夹/demo/src/views/HomeView.vue
@@ -1,26 +1,35 @@
 <template>
-  <div class="home">
+  <div  :class="[flag?'back':'white']">
+      <van-icon name="browsing-history" size="45px" color="#0f9883" @click="flag=!flag"/>
     <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
       <van-swipe-item v-for="item, index in banner">
         <img :src="item.picUrl" alt="">
       </van-swipe-item>
     </van-swipe>
     <h3>商品列表</h3>
-    <ul>
-      <li v-for="item, index in list" :key="item.id" @click="dj(item.id)">
-        <div class="img">
-          <img :src="item.pic" alt="">
-          <span>{{ item.recommendStatusStr }}</span>
-        </div>
-        <p>{{ item.name }}</p>
-        <div class="list-bot">
-          <div class="left">￥{{ item.minPrice }} <s>￥{{ item.originalPrice }}</s></div>
-          <div class="right">
-            <van-button plain type="success" size="mini">购买</van-button>
-          </div>
-        </div>
-      </li>
-    </ul>
+
+
+
+      <van-list v-model:loading="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+        <ul :class="[flag?'back':'white']">
+          <li v-for="item, index in list" :key="item.id" @click="dj(item.id)" :class="[flag?'back':'white']">
+            <div class="img">
+              <img :src="item.pic" alt="">
+              <span >{{ item.recommendStatusStr }}</span>
+            </div>
+            <p :class="[flag?'back':'white']">{{ item.name }}</p>
+            <div class="list-bot">
+              <div class="left">￥{{ item.minPrice }} <s>￥{{ item.originalPrice }}</s></div>
+              <div class="right">
+                <van-button plain type="success" size="mini">购买</van-button>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </van-list>
+    <van-back-top bottom="15vh"/>
+
+
     <mytab></mytab>
   </div>
 </template>
@@ -30,6 +39,9 @@ import { querybanner, querylist } from '@/API/http';
 import { ref, reactive } from 'vue';
 import { useRouter } from 'vue-router';
 import mytab from '@/components/mytab.vue';
+const flag=ref(false)
+const loading = ref(false);
+const finished = ref(false);
 const router = useRouter()
 const types = ref<any>('indexBanner')
 const banner = ref<any>([])
@@ -46,13 +58,32 @@ const params = reactive({
   pageSize: 10,
   page: 1
 })
+
+const onLoad = () => {
+  params.page++
+  setlist()
+};
+
 const setlist = () => {
   querylist(params).then(res => {
-    console.log(res.data.result);
-    list.value = res.data.result
+    console.log(res.data);
+    if (params.page == 1) {
+      list.value = res.data.result
+    } else {
+      list.value = [...list.value, ...res.data.result]
+    }
+
+    if (res.data.totalRow >= list.value.length) {
+      finished.value = true
+    }
+
   })
 }
 setlist()
+
+
+
+
 
 const dj = (id: number) => {
   router.push({
@@ -64,6 +95,22 @@ const dj = (id: number) => {
 }
 </script>
 <style lang="scss" scoped>
+.back{
+  background-color: #000;
+  color: #fff;
+}
+.white{
+  background-color: #fff;
+  color: #000;
+}
+i{
+  position: fixed;
+  bottom: 150px;
+  background-color: #fff;
+  border-radius: 50%;
+  right: 30px;
+    z-index: 999999999;
+  }
 ul {
   width: 100%;
   display: flex;
@@ -72,6 +119,7 @@ ul {
   flex-wrap: wrap;
   background-color: #eee;
   padding-bottom: 50px;
+
   li {
     width: 48%;
     border-radius: 5px;
@@ -146,4 +194,5 @@ ul {
 h3 {
   text-align: center;
   margin: 15px;
-}</style>
+}
+</style>
